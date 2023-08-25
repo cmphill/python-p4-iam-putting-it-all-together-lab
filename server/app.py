f11f2db9b36e345fb3eafe51c64117e2351542c3
Cooper <cmphill@pm.me>
2023-08-25 12:16:35 -0600

    something was not working properly

--- server/app.py
+++ server/app.py
@@ -8,19 +8,95 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+        password_confirmation = data['password_confirmation']
+        image_url = data['image_url']
+        bio = data ['bio']
 
+        if username and password==password_confirmation and image_url and bio :
+            new_user = User(username=username)
+            new_user.password_hash = password
+            new_user.image_url = image_url
+            new_user.bio = bio
+
+            try:
+
+                db.session.add(new_user)
+                db.session.commit()
+
+                session['user_id'] = new_user.id
+
+                return new_user.to_dict(rules=('-_password_hash')), 201
+            except IntegrityError:
+                return {'error': '422 unprocessable entity'}, 422
+        return {'error': '422 unprocessable entity'}, 422
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if session.get('user_id'):
+            user = User.query.filter(User.id == session['user_id']).first()
+
+            return user.to_dict(rules=('_password_hash')), 200
+        
+        return {'error': '401: not authorized'}, 401
 
 class Login(Resource):
-    pass
+    def post(self):
+        request_json = request.get_json()
+        username = request_json.get('username')
+        password = request_json.get('password')
+
+        user = User.query.filter(User.username == username).first()
+
+        if user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+
+        return {'error': '401 unauthorized'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+
+        if session.get('user_id'):
+            session['user_id'] = None
+            return {}, 204
+        
+        return {'error': '401 Unauthorized'}, 401
+        
+    
 
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+        if session.get('user_id'):
+            user = User.query.filter(User.id == session['user_id']).first()
+
+            return [recipe.to_dict() for recipe in user.recipes], 200
+        
+        return {'error': '401 Unauthorized'}, 401
+    
+    def post(self):
+        if session.get('user_id'):
+            data = request.get_json()
+            new_recipe = Recipe(
+                title = data['title'],
+                instructions = data['instructions'],
+                minutes_to_complete = data['minutes_to_complete']
+            )
+            try:
+                db.session.add(new_recipe)
+                db.session.commit()
+
+                return new_recipe.to_dict(), 201
+            
+            except:
+                
+                return {'error': '422 unprocessable'}, 422
+
+        return {'error': '401 not authorized'}
+
+        
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
--- server/models.py
+++ server/models.py
@@ -1,14 +1,53 @@
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
 
 from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True )
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+    recipes = db.relationship("Recipe", back_populates='user')
 
+    serialize_rules=('-recipes.user')
+
+    @hybrid_property
+    def password_hash(self):
+        raise Exception('Password hashes may not be viewed.')
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+        
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8')
+        )
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
     
-    pass
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String, nullable=False)
+    minutes_to_complete = db.Column(db.Integer)
+
+    user = db.relationship("User", back_populates='recipes')
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    serialize_rules = ('-user.recipes')
+
+    @validates('instructions')
+    def validates_instructions(self, key, instructions):
+        if len(instructions) < 50:
+            return ValueError('insufficient instruction length')
+        return instructions
+
